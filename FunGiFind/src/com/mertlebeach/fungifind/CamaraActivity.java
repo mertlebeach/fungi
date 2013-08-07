5e55dc2c13799888b74914ebd18de07862e84948
mertlebeach <etothepietimesi@gmail.com>
2013-08-07 15:40:52 -0700

    This is the fungi find

---
+++ FunGiFind/src/com/mertlebeach/fungifind/CamaraActivity.java
@@ -0,0 +1,22 @@
+package com.mertlebeach.fungifind;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CamaraActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_camara);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.camara, menu);
+		return true;
+	}
+
+}
---
+++ com.mertlebeach.fungifind/src/com/mertlebeach/fungifind/MainActivity.java
@@ -0,0 +1,57 @@
+package com.mertlebeach.fungifind;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+
+import com.mertlebeach.fungifind.R;
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+      Parse.initialize(this, "f9nuFRqE5CjHeXL5DxtKourEaDLM62sDkiYGjLzV", "JSCyGA0ovUs894LhEnqEijDCZgK2kgezoWLghk4T");
+      ParseFacebookUtils.initialize("166475660199140");
+      ParseFacebookUtils.logIn(this, new LogInCallback() {
+    	  public void done(ParseUser user, ParseException err) {
+    	    if (user == null) {
+    	      Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
+    	    } else if (user.isNew()) {
+    	      Log.d("MyApp", "User signed up and logged in through Facebook!");
+    	    } else {
+    	      Log.d("MyApp", "User logged in through Facebook!");
+    	    }
+    	  }
+
+//		@Override
+//		public void done(ParseUser arg0, com.parse.ParseException arg1) {
+//			// TODO Auto-generated method stub
+//			
+//		}
+    	});
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	  super.onActivityResult(requestCode, resultCode, data);
+	  ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
